60b30e0624f40e92f47dd969706c1d7e3e3345e9
Rafid Hoda <rafidhoda@gmail.com>
2024-01-08 12:21:20 +0100

    add push ability to realtime database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
+import { getDatabase,
+         ref,
+         push } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
             
 const firebaseConfig = {
     databaseURL: "https://pebbles-afe23-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -50,8 +52,8 @@ deleteBtn.addEventListener("dblclick", function() {
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    const locationInDatabase = ref(database, "inputs")
+
+    push(locationInDatabase, inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
 })
